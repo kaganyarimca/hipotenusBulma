20ac7014d9cdede4a01d4d1b44ef6d01afbe8199
kaganyarimca <77283330+kaganyarimca@users.noreply.github.com>
2023-09-07 11:11:44 +0300

    added project files

---
+++ hipotenusBulma/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b ;
+        double c;
+        Scanner input = new Scanner((System.in));
+        System.out.println("1. kenarı giriniz: ");
+        a = input.nextInt();
+        System.out.println("2. kenarı giriniz: ");
+        b =input.nextInt();
+
+        c = Math.sqrt((a*a)+ (b*b));
+        System.out.println("Hipotenüs: "+ c );
+
+
+    }
+}
